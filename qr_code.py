5e10bbee028da5f8dd1b1a7fb9e8e662a1de8397
Ragul2345 <raguldevaraj023@gmail.com>
2026-02-03 23:20:04 +0530

    Initial commit - QR Code Generator using Python

---
+++ qr_code.py
@@ -0,0 +1,18 @@
+import qrcode
+
+data = "This is a sample QR code generated using Python"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr_code.png")
+
+print("QR Code generated successfully!")
